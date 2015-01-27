296d3ab59df1df70061c116d6db5d339c0f940fc
wujiwh <wujiwh@gmail.com>
2015-01-28 00:34:28 +0800

    Update dht11_curl.sh

--- dht11_curl.sh
+++ dht11_curl.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+#=============================================================================
+#     FileName: dht11_curl.sh
+#         Desc:
+#       Author: wangheng
+#        Email: wujiwh@gmail.com
+#     HomePage: http://wangheng.org
+#      Version: 0.0.1
+#   LastChange: 2015-01-28 00:30:11
+#      History:
+#=============================================================================
+
 
 #填入yeelink的API KEY
 yeelink_ApiKey='XXX'
